7a5b61b0658fcf32d853ae9bb14d14d2773ff343
Justin Anderson <justin.anderson@ansarada.com>
2019-06-19 21:13:16 +1000

    Updates

---
+++ precache-manifest.6f0118f3a1a6e7b0897c47779b88d454.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9e8df10fd0ae57e607a69fe2e062859",
+    "url": "/lorgnette/index.html"
+  },
+  {
+    "revision": "f3a9ff703200a80a15cd",
+    "url": "/lorgnette/static/css/main.b248f3a7.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/lorgnette/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "f3a9ff703200a80a15cd",
+    "url": "/lorgnette/static/js/main.d11a1835.chunk.js"
+  },
+  {
+    "revision": "f3d49a8e0757b63088a2",
+    "url": "/lorgnette/static/js/runtime~main.29982c2a.js"
+  }
+]);
