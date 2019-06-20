49f9a4ab974df8c0c4c915c909bb389e83521778
Justin Anderson <justin.anderson@ansarada.com>
2019-06-20 13:32:56 +1000

    Updates

--- precache-manifest.4be9dd61f6bb8bc106b5a412b758bd88.js
+++ precache-manifest.4be9dd61f6bb8bc106b5a412b758bd88.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b39de6dee550e8fd915d4045786be8a3",
+    "revision": "98ef1a52ac814a53761070ae83a01598",
     "url": "/lorgnette/index.html"
   },
   {
-    "revision": "c0722fdaef4e1eb0f4cc",
-    "url": "/lorgnette/static/css/main.3a4d2070.chunk.css"
+    "revision": "28ab9bfa1e3ecd395588",
+    "url": "/lorgnette/static/css/main.96eeae16.chunk.css"
   },
   {
     "revision": "ab865bc25b0fa88f570f",
     "url": "/lorgnette/static/js/2.4f9692d0.chunk.js"
   },
   {
-    "revision": "c0722fdaef4e1eb0f4cc",
-    "url": "/lorgnette/static/js/main.16fe98f7.chunk.js"
+    "revision": "28ab9bfa1e3ecd395588",
+    "url": "/lorgnette/static/js/main.70f4fe49.chunk.js"
   },
   {
     "revision": "f3d49a8e0757b63088a2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lorgnette/precache-manifest.b24fc0dcffb65d0379b0489046269964.js"
+  "/lorgnette/precache-manifest.4be9dd61f6bb8bc106b5a412b758bd88.js"
 );
 
 self.addEventListener('message', (event) => {
